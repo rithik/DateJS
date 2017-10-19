effceea6d3f5816b2f9f2c232ec8206901ce38ca
Rithik Yelisetty <rithik@gmail.com>
2017-10-19 15:41:24 -0400

    Package for Pypi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'DateJS',
+    packages = ['DateJS'],
+    version = '0.1',
+    description = 'Python Library that converts Javascript Date objects to Python Datetime objects Edit', 
+    author = 'Rithik Yelisetty',
+    url = 'https://github.com/rithik/DateJS',
+    download_url = 'https://github.com/rithik/DateJS/archive/0.1.tar.gz',
+    keywords = ['date', 'javascript', 'js', 'conversion', 'timezone', 'time'], 
+    classifiers = [],
+)
